c4f379900caa84dd3790a11f6894ad1684b283a6
Matt Ensor <matt.ensor@redbubble.com>
2016-11-07 18:52:29 +1100

    WIP

--- csv_reader.rb
+++ csv_reader.rb
@@ -1,4 +1,12 @@
+require 'csv'
+
 class CsvReader
-  def read
+  def read(*keys)
+    csv = CSV.new(File.read('users.csv'), headers: true, header_converters: :symbol).to_a
+    csv.each do |line|
+      values = line.to_h.select { |key| keys.include?(key) }
+
+      yield **values if block_given?
+    end
   end
 end
--- parser.rb
+++ parser.rb
@@ -2,9 +2,33 @@ require_relative 'csv_reader'
 
 class Parser
   def parse
-    p "Parsing!!!"
-    CsvReader.new.read do |name|
-      p "name #{name}"
+    c = combine.curry.("hi there ")
+
+    apply_change = -> (fn, name) { fn.call(name); }
+    change_a_to_z = -> (name) { name.gsub("a", "z") }
+    change_thil = -> (name) { name.gsub("thil", "god") }
+
+    apply_a_to_z = apply_change.curry.call(change_a_to_z)
+    thil_is_god = apply_change.curry.call(change_thil)
+
+    CsvReader.new.read(:name, :age, :location) do |age:, name:, location:|
+      p thil_is_god.(apply_a_to_z.(name))
     end
+  end
+
+  def combine
+    -> (template, age) { template + age }
+  end
+
+  def max
+    -> (name, age) { return name if age < 30 }
+  end
+
+  def render_proc(name, age, location)
+    Proc.new { return "name #{name} #{age} #{location}" }
+  end
+
+  def render_lambda(name, age, location)
+    -> { return "name #{name} #{age} #{location}" }
   end
 end
